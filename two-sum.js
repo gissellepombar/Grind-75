d50d9337b63325060d8f4bd2b5b53fecf9b3e346
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-01-22 22:53:15 -0500

    solved two sum with O(n)

--- two-sum.js
+++ two-sum.js
@@ -7,9 +7,23 @@
 function twoSum (nums, target) {
     for (let i = 0; i < nums.length; i++){
         for (let j = i + 1; j < nums.length; j++){
-        if (target == nums[i] + nums[j]){
+        if (nums[i] + nums[j] === target){
             return [i, j];
         }
     }
 } 
 }
+//O(n^2)
+
+function twoSum2(nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map[complement] !== undefined) {
+            return [map[complement], i];
+        }
+        map[nums[i]] = i;
+    }
+}
+
+//O(n)
