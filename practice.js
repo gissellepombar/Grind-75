95c47d8e1a2afc0d8276def71e9c4ef2d956e08d
Gisselle Pombar <gisselle.pombar@gmail.com>
2023-01-24 22:49:45 -0500

    updated valid-parentheses solution

---
+++ practice.js
@@ -0,0 +1,34 @@
+// Here is an array:
+//const yummies = ["apple", "blueberry", "cantaloupe",  "dates"]
+
+//  Write the code to generate an array that looks like ["dates","apple","blueberry"]
+
+const yummies = ["apple", "blueberry", "cantaloupe",  "dates"]
+const copyYummies = [...yummies];
+
+const modifyArr = (arr) => {
+    const lastItem = copyYummies.pop()
+    copyYummies.unshift(lastItem)
+    copyYummies.pop()
+};
+
+modifyArr(copyYummies);
+console.log(copyYummies);
+
+let vegetableArray = ["zucchini", "carrot", "lettuce", "potato"]
+
+const vegetable = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+};
+vegetable(vegetableArray);
+
+
+let color = "";
+if (color === "red") { console.log ("Happy Lunar New Year!!"); } else { console.log("Colors are cool!"); }
+
+let color = "";
+color === 'red' ? console.log("Happy Lunar New Year!!") : console.log("Colors are cool!");
+
+
